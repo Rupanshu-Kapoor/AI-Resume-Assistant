6f07474d1c124f1b0e98810d197247215a3d8849
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-06-01 17:41:16 +0530

    Add config file

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, '..', 'data', 'uploads')
+ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
+
+
+
+data_science_skills = ['queries', 'beautifulsoup', 'ms excel', 'mathematics', 'selenium', 'html', 'analytical skills', 'statsmodels','ai', 'improvement', 'analyze', 'metrics', 'forecasting', 'analytics', 'analytical', 'mysql', 'postgresql', 'database', 'writing', 'excel','regulations', 'algorithms', 'scipy', 'opencv', 'reports',  'eda', 'jupyter', 'presentations', 'modeling', 'audit', 'technical skills', 'schedule', 'nltk', 'iso', 'xgboost', 'segmentation', 'github', 'seaborn', 'keras', 'distribution', 'investigation', 'tableau','probability', 'analysis', 'r', 'technical', 'programming', 'web scraping', 'research', 'pandas', 'statistical analysis', 'numpy', 'predictive analysis', 'tensorflow', 'hypothesis', 'matplotlib', 'scikit-learn', 'information technology', 'machine learning', 'cloud', 'streamlit', 'mining', 'python', 'data analytics', 'deep learning', 'testing', 'training', 'clustering & classification', 'data analysis', 'engineering', 'data visualization', 'quantitative analysis', 'statistics', 'flask', 'statistical modeling', 'pytorch', 'data mining', 'aws', 'sql']
