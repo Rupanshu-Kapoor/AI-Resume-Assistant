9dc164dd85d8b9cf9aa6c1bfe5cb7a727e63ce4a
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-06-01 17:52:17 +0530

    API to review resume

---
+++ src/reviewer.py
@@ -0,0 +1,19 @@
+import re
+from pdfminer.high_level import extract_text
+import logging
+
+
+class ResumeReviewer:
+    def __init__(self):
+        pass
+
+    def review(self):
+        pass
+
+    def grammar_check(self, text):
+        # split the text into lines
+        lines = text.splitlines()
+
+        # check for grammer mistakes in each line
+        # for line in lines:
+            
